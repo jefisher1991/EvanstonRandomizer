a3171d8047d1196e41bea8df8c88592d07139d0d
Jeannine Hall <jeanninehall@dhcp-10-105-183-37.wireless.northwestern.private>
2017-07-10 19:55:22 -0500

    save button working

--- app/components/data.js
+++ app/components/data.js
@@ -1,13 +1,12 @@
-
 var axios = require('axios');
 
 
 var API = "AIzaSyC585Jhr1eNvtH19TWMycNAZgthBaciGvA";
-var evanston = new google.maps.LatLng(42.0451, -87.6877);
+var evanston = new google.maps.LatLng(42.0483, -87.6821);
 
 var map = new google.maps.Map(document.getElementById('app'), {
   center: evanston,
-  zoom: 15
+  zoom: 5
 });
 
 const service = new google.maps.places.PlacesService(map);
@@ -21,12 +20,22 @@ var helpers = {
 		// remember to swap location param with location key in object!
 		const request =  {
 		    location: evanston,
-		    radius: '1000',
+		    radius: '450',
 		    query: 'restaurant'
  		};
 
 		service.textSearch(request, callback);
-	}
+	},
+
+	postSaved: function(name, formatted_address) {
+    	var newRestaurant = { restaurantName: name, location: formatted_address};
+    	return axios.post("/api/saved", newRestaurant)
+      .then(function(response) {
+        console.log("axios results", response.data._id);
+        return response.data._id;
+      });
+  	}
+
 
 }
 
